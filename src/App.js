3e56b7f4b85de84f93ea95521eba9b7971097087
Nick Cavaliere <nickcavaliere1991@gmail.com>
2021-10-04 17:14:16 -0400

    fix margins

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Login from './Login';
 function App() {
   return (
     <div class="flex flex-col min-h-screen min-w-screen items-center justify-center">
-      <h1 class="font-extrabold text-6xl p-2">CRUMZ</h1>
-      <h1 class="font-extrabold text-4xl p-">Heading</h1>
-      <p class="m-4">OR</p>
-      <div class="mb-4 w-full text-center flex justify-evenly">
+      <h1 class="font-extrabold text-6xl m-11">CRUMZ</h1>
+      <h1 class="font-extrabold text-4xl mb-6">Heading</h1>
+      <div class="m-4 w-full text-center flex justify-evenly">
         <button class=" text-white bg-blue-800 py-2 px-4 w-1/3">Facebook Button</button>
         <button class=" text-red bg-green-500 py-2 px-4 w-1/3">Google Button</button>
       </div>
+      <p class="m-4">OR</p>
       <Login />
     </div>
   );
