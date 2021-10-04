10ffcf2597158477aa1d493b6461fa78eb660e58
Nick Cavaliere <nickcavaliere1991@gmail.com>
2021-10-04 17:23:46 -0400

    clean up

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
 
 export default function Login() {
@@ -19,7 +17,7 @@ export default function Login() {
     return (
 
         <div className="login w-10/12">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label class="block text-left my-1">Email or Phone Number
                     <input class="border-black block w-full shadow-xl" type="email" value={email} onChange={(e) => setEmail(e.target.value)}>
                     </input>
@@ -32,7 +30,7 @@ export default function Login() {
                 <label class="mt-3 px-2">Remember Me</label>
                 <p class="mt-4 text-center">New to <b>CRUMZ</b>? <a href="#" class="text-blue-700">Sign up Here!</a></p>
                 <p class="mb-7 text-center">Are you a business owner?<a class="text-blue-700" href="#"> Click Here</a></p>
-                <button class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full my-4">Login</button>
+                <button disabled={!validateForm} class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full my-4">Login</button>
             </form>
         </div>
     )
